9e8a760437daae9e3319d312d9bbe2e216a62073
Khaled Elsherif <Khaled.Elsayed@mdp.global>
2025-07-12 01:50:15 +0300

    fix

--- src/pages/subscriptionPlans.tsx
+++ src/pages/subscriptionPlans.tsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Plus } from 'lucide-react';
 import { fetchSubscriptions, createSubscription, clearError, clearSuccess } from '../store/slices/subscriptionSlice';
